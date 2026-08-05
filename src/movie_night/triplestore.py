8916e74cf1a2a251f7160236ca2186aacb9c0412
e2dubba <svirgilgoodwin@gmail.com>
2026-08-04 21:38:12 -0600

    add movienode and sort the date for the movie search

--- src/movie_night/triplestore.py
+++ src/movie_night/triplestore.py
@@ -180,24 +180,25 @@ def find_movies(user_node: str , member_node: str | NamedNode ):
     """
     mem_node = member_node if isinstance(member_node, NamedNode) else NamedNode(member_node)
     query = """PREFIX mno:  <https://ontology.movie-night.site/>
-    SELECT ?movie_title ?poster_url ?date
+    SELECT ?movie_node ?movie_title ?poster_url ?date
     FROM mno:MovieGraph
     FROM %s
     WHERE {
         %s
             mno:choice [
                 mno:onDate ?date ;
-                mno:selection ?movieNode ;
+                mno:selection ?movie_node ;
             ] ;
         .
-        ?movieNode
+        ?movie_node
             mno:title ?movie_title ;
             mno:poster ?poster_url ;
         .
     }
+    ORDER BY DESC(?date)
     """ % (NamedNode(user_node), mem_node)
 
-    choices = [{"movieTitle": str(movie_title), "posterUrl": poster_url.value, "date": date.value } for movie_title, poster_url, date in store.query(query) ]
+    choices = [{"movieNode": movie_node.value, "movieTitle": movie_title.value, "posterUrl": poster_url.value, "date": date.value } for movie_node, movie_title, poster_url, date in store.query(query) ]
     return choices
 
 
